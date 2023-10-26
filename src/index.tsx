b4357c697cca0e53592d98da7464f3eec2bcf09c
Baryshewww <baryshev-tm@mail.ru>
2023-10-26 17:19:38 +0300

    Роутинг Code splitting Lazy Suspence

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import someFn from './test'
 import {render} from "react-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
 someFn(123);
 
 render(
-    <App/>,
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
     document.getElementById('root')
 )
