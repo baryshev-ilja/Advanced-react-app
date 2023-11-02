c8007473c65f6badd4a2a6a8715b9e6dc27a7db0
Baryshewww <baryshev-tm@mail.ru>
2023-11-02 12:53:48 +0300

    Svg loader. File loader. Button UI kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './styles/index.scss';
-import {Link} from "react-router-dom";
 import {classNames} from 'shared/lib/classNames/classNames';
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <button onClick={toggleTheme}>Toggle</button>
 
             <AppRouter/>
         </div>
