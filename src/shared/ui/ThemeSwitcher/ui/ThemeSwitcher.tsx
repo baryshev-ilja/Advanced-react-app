7955834d4eb184a054686626062fbe1667917cc6
Baryshewww <baryshev-tm@mail.ru>
2023-11-03 19:43:33 +0300

    Stylelint. Plugin for i18next

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -14,6 +15,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
         <Button
             theme={ThemeButton.CLEAR}
             onClick={toggleTheme}
+            className={classNames('', {}, [className])}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
