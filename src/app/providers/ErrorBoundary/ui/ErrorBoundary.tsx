4a7ee673ab41c54f1cdd4ac7288ca4ed89a4a235
Baryshewww <baryshev-tm@mail.ru>
2023-11-11 18:01:31 +0300

    ErrorBoundary. Обработка React ошибок

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+import { PageError } from 'widgets/PageError/PageError';
+
+interface ErrorBoundaryProps {
+    children?: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary
+    extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo) {
+        // eslint-disable-next-line no-console
+        console.log(error, info.componentStack);
+    }
+
+    render() {
+        const { children } = this.props;
+        const { hasError } = this.state;
+
+        if (hasError) {
+            return (
+                <Suspense fallback="">
+                    <PageError />
+                </Suspense>
+            );
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
