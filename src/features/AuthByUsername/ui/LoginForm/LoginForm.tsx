2d33fc29167fc42b3497adb7be258c7c6c9152ab
Baryshewww <baryshev-tm@mail.ru>
2024-01-11 15:05:22 +0300

    feat(components): добавить в форме входа асинхронный запрос к серверу
    - Добавил в сторе слайс отвечающий за авторизацию
    - В компоненте LoginForm написал обработчики событий: успешная авторизация, не успешная авторизация
    - Добавил в форму входа заголовок из нового компонента Text

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -2,36 +2,52 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
-import { useState } from 'react';
+import { memo, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Text, ThemeText } from 'shared/ui/Text/Text';
+import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { loginActions } from '../../model/slice/loginSlice';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string;
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const {
+        username,
+        password,
+        isLoading,
+        error,
+    } = useSelector(getLoginState);
 
-    const onChangeLogin = (val: string) => {
-        setLogin(val);
-    };
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
 
-    const onChangePass = (val: string) => {
-        setPassword(val);
-    };
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+        dispatch(loginByUsername({ username, password }));
+    }, [dispatch, password, username]);
 
     const labelUsername = t('Введите логин >');
     const labelPassword = t('Введите пароль >');
 
     return (
         <div className={classNames(cls.loginForm, {}, [className])}>
+            <Text title={t('Форма авторизации')} />
+            {error && <Text description={error} theme={ThemeText.ERROR} />}
             <Input
                 type="text"
                 className={cls.input}
-                value={login}
-                onChange={onChangeLogin}
+                value={username}
+                onChange={onChangeUsername}
                 labelElement={labelUsername}
                 autofocus
                 id="username"
@@ -40,16 +56,18 @@ export const LoginForm = ({ className }: LoginFormProps) => {
                 type="text"
                 className={cls.input}
                 value={password}
-                onChange={onChangePass}
+                onChange={onChangePassword}
                 labelElement={labelPassword}
                 id="password"
             />
             <Button
                 className={cls.btn}
                 theme={ButtonTheme.OUTLINE}
+                onClick={onLoginClick}
+                disabled={isLoading}
             >
                 {t('Войти')}
             </Button>
         </div>
     );
-};
+});
