6e83320138497d43f4f448b43c051e1543057c07
Baryshewww <baryshev-tm@mail.ru>
2024-01-16 13:48:15 +0300

    refactor(state): разбить общий селектор на несколько более конкретных
    - Один общий селектор getLoginState разбил на более конкретные селекторы:
    --- getLoginUsername (поле username)
    --- getLoginPassword (поле password)
    --- getLoginLoading (поле IsLoading)
    --- getLoginError (поле error)

---
+++ src/features/AuthByUsername/model/selectors/getLoginStateError/getLoginError.ts
@@ -0,0 +1,7 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getLoginState } from '../getLoginState/getLoginState';
+
+export const getLoginError = createSelector(
+    getLoginState,
+    (loginFormState) => loginFormState?.error,
+);
---
+++ src/features/AuthByUsername/model/selectors/getLoginStateLoading/getLoginLoading.ts
@@ -0,0 +1,7 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { getLoginState } from '../getLoginState/getLoginState';
+
+export const getLoginLoading = createSelector(
+    getLoginState,
+    (loginFormState) => loginFormState?.isLoading || false,
+);
