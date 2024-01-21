74c782b0b68232b213af3d815739bdc9d7f1c3de
Baryshewww <baryshev-tm@mail.ru>
2024-01-21 17:15:48 +0300

    refactor(feature): изменить названия переменных на более логичные
    - Изменил названия переменных в классе TestAsyncThunk на более логичные, чтобы по названию сразу было понятно что делает код

--- src/shared/lib/tests/testAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/testAsyncThunk/TestAsyncThunk.ts
@@ -18,9 +18,9 @@ export class TestAsyncThunk<Return, Arg, RejectedValue> {
     }
 
     async callThunk(arg: Arg) {
-        const action = this.actionCreator(arg);
-        const result = await action(this.dispatch, this.getState, undefined);
+        const actionCreator = this.actionCreator(arg);
+        const action = await actionCreator(this.dispatch, this.getState, undefined);
 
-        return result;
+        return action;
     }
 }
