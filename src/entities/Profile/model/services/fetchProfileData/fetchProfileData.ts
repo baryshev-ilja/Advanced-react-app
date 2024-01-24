5c6855e97409061eccb1a4109d97a6fc1794dc1c
Baryshewww <baryshev-tm@mail.ru>
2024-01-24 14:01:20 +0300

    feat(components): добавить компонент ProfileCard и логику отображения
    - Создал новый компонент ProfileCard (карточка данных профиля)
    - Создал slice с асинхронной фичей получения данных профиля с сервера
    - Создал селекторы для получения данных из стейта

---
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfigApi } from 'app/providers/StoreProvider';
+import { Profile } from '../../types/profile';
+
+export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfigApi<string>>(
+    'profile/fetchProfileData',
+    async (_, thunkAPI) => {
+        const { extra, rejectWithValue } = thunkAPI;
+
+        try {
+            const response = await extra.api.get<Profile>('/profile');
+
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return rejectWithValue('error');
+        }
+    },
+);
---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,47 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui/Text/Text';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useSelector } from 'react-redux';
+import { getProfileData, getProfileError, getProfileLoading } from 'entities/Profile';
+import { Input } from 'shared/ui/Input/Input';
+import cls from './ProfileCard.module.scss';
+
+interface ProfileCardProps {
+    className?: string;
+}
+
+export const ProfileCard = ({ className }: ProfileCardProps) => {
+    const { t } = useTranslation('profile');
+
+    const data = useSelector(getProfileData);
+    const isLoading = useSelector(getProfileLoading);
+    const error = useSelector(getProfileError);
+
+    return (
+        <div className={classNames(cls.profileCard, {}, [className])}>
+            <div className={cls.header}>
+                <Text title={t('Профиль')} />
+                <Button
+                    className={cls.editBtn}
+                    theme={ButtonTheme.OUTLINE}
+                >
+                    {t('Редактировать')}
+                </Button>
+            </div>
+            <div className={cls.data}>
+                <Input
+                    className={cls.input}
+                    value={data?.first}
+                    labelElement={t('Ваше имя')}
+                />
+                <Input
+                    className={cls.input}
+                    value={data?.lastname}
+                    labelElement={t('Ваша фамилия')}
+                />
+            </div>
+
+        </div>
+    );
+};
