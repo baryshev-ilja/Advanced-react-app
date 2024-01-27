c99574e2a1c8119e42b8b004b63c3fa922683fcc
Baryshewww <baryshev-tm@mail.ru>
2024-01-27 19:51:38 +0300

    docs(storybook): добавить сторисы для сайдбара когда нет авторизации
    - Добавил новые сторисы для состояния Sidebar при неавторизованном пользователе (ссылка на страницу профиля будет отсутствовать)

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -16,7 +17,24 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    user: { authData: {} },
+})];
+
+export const NoAuthLight = Template.bind({});
+NoAuthLight.args = {};
+NoAuthLight.decorators = [StoreDecorator({
+    user: {},
+})];
+
+export const NoAuthDark = Template.bind({});
+NoAuthDark.args = {};
+NoAuthDark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    user: {},
+})];
