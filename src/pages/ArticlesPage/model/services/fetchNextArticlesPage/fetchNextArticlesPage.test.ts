d780361ffc1bbc35d413a034c2ee5c26e98a0d28
Baryshewww <baryshev-tm@mail.ru>
2024-02-20 11:00:04 +0300

    test(thunks): добавить тесты для thunk-a fetchNextArticlesPage

---
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
@@ -0,0 +1,58 @@
+import { TestAsyncThunk } from 'shared/lib/tests/testAsyncThunk/TestAsyncThunk';
+import { fetchNextArticlesPage } from './fetchNextArticlesPage';
+import { fetchArticleList } from '../fetchArticleList/fetchArticleList';
+
+jest.mock('../fetchArticleList/fetchArticleList');
+
+describe('fetchNextArticlesPage.test', () => {
+    test('success fetching', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlesPage: {
+                page: 1,
+                hasMore: true,
+                ids: [],
+                entities: {},
+                isLoading: false,
+            },
+        });
+
+        const action = await thunk.callThunk();
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(4);
+        expect(fetchArticleList).toHaveBeenCalledWith({ page: 2 });
+    });
+
+    test('fetchNextArticlesPage not called', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlesPage: {
+                page: 1,
+                hasMore: false,
+                ids: [],
+                entities: {},
+                isLoading: false,
+            },
+        });
+
+        const action = await thunk.callThunk();
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(fetchArticleList).not.toHaveBeenCalled();
+    });
+
+    test('fetchNextArticlesPage is loading', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlesPage: {
+                page: 1,
+                hasMore: false,
+                ids: [],
+                entities: {},
+                isLoading: true,
+            },
+        });
+
+        const action = await thunk.callThunk();
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(fetchArticleList).not.toHaveBeenCalled();
+    });
+});
