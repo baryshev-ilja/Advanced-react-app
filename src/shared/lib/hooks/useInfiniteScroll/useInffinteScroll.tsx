3fdf87e9f450043892087e97ad5d612b2eba36ef
Baryshewww <baryshev-tm@mail.ru>
2024-02-20 16:01:47 +0300

    fix(hooks): исправить ошибку в хуке useInfiniteScroll
    - Замкнул wrapperRef и triggerRef  внутри хука useEffect. Засчет этого перестала появляться ошибка, потому что хук стал запоминать значения этих переменных, которые передаются извне

--- src/shared/lib/hooks/useInfiniteScroll/useInffinteScroll.tsx
+++ src/shared/lib/hooks/useInfiniteScroll/useInffinteScroll.tsx
@@ -10,11 +10,13 @@ export function useInfiniteScroll(props: useInfiniteScrollOptions) {
     const { triggerRef, wrapperRef, callback } = props;
 
     useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
         let observer: IntersectionObserver | null = null;
 
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '1px',
                 threshold: 1.0,
             };
@@ -23,13 +25,13 @@ export function useInfiniteScroll(props: useInfiniteScrollOptions) {
                 if (entry.isIntersecting) callback();
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
         }
 
         return () => {
-            if (observer) {
+            if (observer && triggerElement) {
                 // eslint-disable-next-line
-                observer.unobserve(triggerRef.current);
+                observer.unobserve(triggerElement);
             }
         };
     }, [callback, triggerRef, wrapperRef]);
