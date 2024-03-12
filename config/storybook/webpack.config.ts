b8477131c3e4cd65791fda27ba8fd6cd593a604c
Baryshewww <baryshev-tm@mail.ru>
2024-03-12 16:02:36 +0300

    refactor(config): добавить поддержку алиасов в конфиги

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -18,6 +18,7 @@ export default ({ config }: {config: webpack.Configuration}) => {
     // config.resolve.modules = [paths.src, 'node_modules'];
     config!.resolve!.modules!.unshift(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = { '@': paths.src };
 
     // eslint-disable-next-line no-param-reassign
     const rules = config!.module!.rules as RuleSetRule[];
