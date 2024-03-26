43d11ce94381d1c7a9633a093f6c89ce2dc163a9
Baryshewww <baryshev-tm@mail.ru>
2024-03-26 15:43:14 +0300

    test(server): проверить работу CI/CD (6)

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,5 +2,5 @@ cd ~/Advanced-react-app
 git pull
 npm run build:prod
 
-cd rm -rf /var/www/Advanced-react-app/html
+rm -rf /var/www/Advanced-react-app/html
 mv build /var/www/Advanced-react-app/html
