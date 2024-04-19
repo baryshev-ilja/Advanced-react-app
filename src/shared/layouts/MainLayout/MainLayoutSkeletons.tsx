3734bf69e5f45adb53fdc4b461f4b2300c7af605
Baryshewww <baryshev-tm@mail.ru>
2024-04-19 10:58:30 +0300

    feat(ui): создать скелетоны для главного лайаута
    - Создал скелетоны которые отображаются при первой отрисовке приложения

---
+++ src/shared/layouts/MainLayout/MainLayoutSkeletons.tsx
@@ -0,0 +1,47 @@
+import { memo } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+import {
+    ContentPageSkeleton,
+    RightbarSkeleton,
+    SidebarSkeleton,
+    Skeleton,
+} from '@/shared/ui/redesigned/Skeleton';
+import { HStack } from '@/shared/ui/redesigned/Stack';
+
+import cls from './MainLayout.module.scss';
+import clsSkeletons from './MainLayoutSkeletons.module.scss';
+
+export const MainLayoutSkeletons = memo(() => {
+    return (
+        <div className={classNames(cls.mainLayout, {}, [clsSkeletons.layout])}>
+            <div className={classNames(cls.header, {}, [clsSkeletons.header])}>
+                <div className={clsSkeletons.navbar}>
+                    <HStack justify="between" align="center" className={clsSkeletons.navbarInner}>
+                        <HStack gap="16" align="center">
+                            <Skeleton width={45} height={45} borderRadius="8px" />
+                            <Skeleton width={300} height={20} borderRadius="16px" />
+                        </HStack>
+                        <Skeleton width={102} height={40} borderRadius="8px" />
+                    </HStack>
+                </div>
+            </div>
+            <div className={classNames(cls.contentWrapper, {}, [clsSkeletons.contentWrapper])}>
+                <div className={cls.contentInner}>
+                    <div className={cls.content}>
+                        <ContentPageSkeleton />
+                    </div>
+                    <div className={classNames(cls.sidebar, {}, [clsSkeletons.sidebarSkeleton])}>
+                        <SidebarSkeleton />
+                    </div>
+                    <div className={classNames(cls.rightbar, {}, [clsSkeletons.rightbarSkeleton])}>
+                        <RightbarSkeleton />
+                    </div>
+                    <div className={cls.toolbar}>
+                        <Skeleton width={40} height={40} borderRadius="50%" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+});
---
+++ src/shared/ui/redesigned/Skeleton/RightbarSkeleton/RightbarSkeleton.tsx
@@ -0,0 +1,31 @@
+import { memo } from 'react';
+
+// eslint-disable-next-line baryshewww/layers-import
+
+import { CardUI } from '../../CardUI';
+import { VStack } from '../../Stack';
+import { Skeleton } from '../Skeleton';
+
+import cls from './RightbarSkeleton.module.scss';
+
+export const RightbarSkeleton = memo(() => {
+    return (
+        <div className={cls.rightbarSkeleton}>
+            <CardUI className={cls.uiBlock} padding="16" gap="16" borderRadius="16">
+                <VStack gap="8">
+                    <Skeleton width={50} height={19} borderRadius="16px" />
+                    <Skeleton width="100%" height={40} borderRadius="8px" />
+                </VStack>
+                <VStack gap="8">
+                    <Skeleton width={159} height={19} borderRadius="16px" />
+                    <Skeleton width={70} height={40} borderRadius="32px" />
+                </VStack>
+                <VStack gap="8">
+                    <Skeleton width={127} height={19} borderRadius="16px" />
+                    <Skeleton width="100%" height={40} borderRadius="8px" />
+                    <Skeleton width="100%" height={40} borderRadius="8px" />
+                </VStack>
+            </CardUI>
+        </div>
+    );
+});
