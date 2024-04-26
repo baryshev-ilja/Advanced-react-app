f4976a2f5b7ab581929ac52da1ed6cee76c3fec7
Baryshewww <baryshev-tm@mail.ru>
2024-04-26 09:45:18 +0300

    feat(ui): разбить на старый/новый дизайн компонент ProfileCard

--- src/entities/profile/index.ts
+++ src/entities/profile/index.ts
@@ -1,3 +1,5 @@
 export type { Profile, ProfileSchema } from './model/types/profile';
 export { ValidateProfileError } from './model/consts/constsProfile';
 export { ProfileCard } from './ui/ProfileCard/ProfileCard';
+export { ProfileCardSkeleton } from './ui/ProfileCardRedesigned/ProfileCardSkeleton';
+export { ProfilePageSkeleton } from './ui/ProfileCardRedesigned/ProfilePageSkeleton';
