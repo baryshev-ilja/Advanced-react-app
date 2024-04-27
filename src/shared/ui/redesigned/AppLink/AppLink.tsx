30bce268a256e505e72478af3b373603cfec6bd9
Baryshewww <baryshev-tm@mail.ru>
2024-04-27 16:12:11 +0300

    refactor(ui): добавить выбор цвета шрифта для ссылки AppLink

--- src/shared/ui/redesigned/AppLink/AppLink.tsx
+++ src/shared/ui/redesigned/AppLink/AppLink.tsx
@@ -1,14 +1,17 @@
 import { ForwardedRef, forwardRef, ReactNode } from 'react';
 import { LinkProps, NavLink } from 'react-router-dom';
 
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { AdditionalCls, classNames } from '@/shared/lib/classNames/classNames';
 
 import cls from './AppLink.module.scss';
+
+type VariantLink = 'linkUi';
 
 interface AppLinkProps extends LinkProps{
     className?: string;
     children?: ReactNode;
     activeClassName?: string;
+    variant?: VariantLink;
 }
 
 export const AppLink = forwardRef((props: AppLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
@@ -17,14 +20,20 @@ export const AppLink = forwardRef((props: AppLinkProps, ref: ForwardedRef<HTMLAn
         className,
         activeClassName = '',
         children,
+        variant = '',
         ...otherProps
     } = props;
+
+    const additionalCls: AdditionalCls = [
+        className,
+        cls[variant],
+    ];
 
     return (
         <NavLink
             to={to}
             className={
-                ({ isActive }) => classNames(cls.appLink, { [activeClassName]: isActive }, [className])
+                ({ isActive }) => classNames(cls.appLink, { [activeClassName]: isActive }, additionalCls)
             }
             ref={ref}
             {...otherProps}
