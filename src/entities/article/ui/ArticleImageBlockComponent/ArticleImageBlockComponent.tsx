b77068c1e22afb89900910feee04735c27bc4807
Baryshewww <baryshev-tm@mail.ru>
2024-04-30 17:11:19 +0300

    refactor(ui): обновить дизайн для ArticleImageBlockComponent

--- src/entities/article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,7 +1,10 @@
 import { ArticleImageBlock } from '../../model/types/article';
 
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextAlign } from '@/shared/ui/deprecated/Text';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Text as TextDeprecated, TextAlign } from '@/shared/ui/deprecated/Text';
+import { VStack } from '@/shared/ui/redesigned/Stack';
+import { Text as TextRedesigned } from '@/shared/ui/redesigned/Text';
 
 import cls from './ArticleImageBlockComponent.module.scss';
 
@@ -13,12 +16,37 @@ interface ArticleImageBlockComponentProps {
 export const ArticleImageBlockComponent = (props: ArticleImageBlockComponentProps) => {
     const { className, block } = props;
     return (
-        <div className={classNames(cls.articleImageBlockComponent, {}, [className])}>
-            <img
-                src={block.src}
-                alt={block.title}
-            />
-            {block.title && <Text description={block.title} align={TextAlign.CENTER} />}
-        </div>
+        <ToggleFeatures
+            name="isAppRedesigned"
+            on={(
+                <VStack
+                    max
+                    className={classNames(cls.articleImageBlockComponent, {}, [className])}
+                    gap="8"
+                >
+                    <img
+                        src={block.src}
+                        alt={block.title}
+                    />
+                    {block.title && (
+                        <TextRedesigned
+                            ui={block.title}
+                            variant="ui"
+                            align="center"
+                            size="sizeS"
+                        />
+                    )}
+                </VStack>
+            )}
+            off={(
+                <div className={classNames(cls.articleImageBlockComponent, {}, [className])}>
+                    <img
+                        src={block.src}
+                        alt={block.title}
+                    />
+                    {block.title && <TextDeprecated description={block.title} align={TextAlign.CENTER} />}
+                </div>
+            )}
+        />
     );
 };
