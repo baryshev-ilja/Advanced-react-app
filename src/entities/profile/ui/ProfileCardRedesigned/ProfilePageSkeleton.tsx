a6c36fe26b1d8e4fbbc354d38a44435b640dd579
Baryshewww <baryshev-tm@mail.ru>
2024-05-02 21:12:39 +0300

    fix(skeletons): исправить ошибки в отображении скелетонов для лайаута

--- src/entities/profile/ui/ProfileCardRedesigned/ProfilePageSkeleton.tsx
+++ src/entities/profile/ui/ProfileCardRedesigned/ProfilePageSkeleton.tsx
@@ -3,6 +3,7 @@ import { ProfileCardSkeleton } from '../../ui/ProfileCardRedesigned/ProfileCardS
 import { StickyContentLayout } from '@/shared/layouts/StickyContentLayout';
 import { CardUI } from '@/shared/ui/redesigned/CardUI';
 import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 
 import cls from './ProfileCardRedesigned.module.scss';
 
@@ -15,10 +16,26 @@ export const ProfilePageSkeleton = () => {
                 </div>
             )}
             rightbar={(
-                <CardUI className={cls.cardSkeleton} padding="16" gap="16" borderRadius="16">
-                    <Skeleton height={40} borderRadius="8px" />
-                    <Skeleton height={40} borderRadius="8px" />
-                </CardUI>
+                <VStack gap="16">
+                    <CardUI className={cls.cardSkeleton} padding="16" gap="16" borderRadius="16">
+                        <Skeleton width="100%" height={40} borderRadius="8px" />
+                        <Skeleton width="100%" height={40} borderRadius="8px" />
+                        <VStack gap="8">
+                            <Skeleton width="100%" height={24} borderRadius="16px" />
+                            <Skeleton width="100%" height={40} borderRadius="8px" />
+                        </VStack>
+                    </CardUI>
+                    <CardUI className={cls.cardSkeleton} padding="16" gap="16" borderRadius="16">
+                        <VStack gap="8">
+                            <Skeleton width={180} height={24} borderRadius="16px" />
+                            <Skeleton width="100%" height={40} borderRadius="8px" />
+                        </VStack>
+                        <VStack gap="8">
+                            <Skeleton width={150} height={24} borderRadius="16px" />
+                            <Skeleton width="100%" height={40} borderRadius="8px" />
+                        </VStack>
+                    </CardUI>
+                </VStack>
             )}
         />
     );
--- src/shared/ui/redesigned/Skeleton/RightbarSkeleton/RightbarSkeleton.tsx
+++ src/shared/ui/redesigned/Skeleton/RightbarSkeleton/RightbarSkeleton.tsx
@@ -2,8 +2,6 @@ import { memo } from 'react';
 
 // eslint-disable-next-line baryshewww/layers-import
 
-import { CardUI } from '../../CardUI';
-import { VStack } from '../../Stack';
 import { Skeleton } from '../Skeleton';
 
 import cls from './RightbarSkeleton.module.scss';
@@ -11,21 +9,7 @@ import cls from './RightbarSkeleton.module.scss';
 export const RightbarSkeleton = memo(() => {
     return (
         <div className={cls.rightbarSkeleton}>
-            <CardUI className={cls.uiBlock} padding="16" gap="16" borderRadius="16">
-                <VStack gap="8">
-                    <Skeleton width={50} height={24} borderRadius="16px" />
-                    <Skeleton width="100%" height={40} borderRadius="8px" />
-                </VStack>
-                <VStack gap="8">
-                    <Skeleton width={159} height={24} borderRadius="16px" />
-                    <Skeleton width={70} height={40} borderRadius="32px" />
-                </VStack>
-                <VStack gap="8">
-                    <Skeleton width={127} height={24} borderRadius="16px" />
-                    <Skeleton width="100%" height={40} borderRadius="8px" />
-                    <Skeleton width="100%" height={40} borderRadius="8px" />
-                </VStack>
-            </CardUI>
+            <Skeleton className={cls.uiBlock} width="100%" height={328} borderRadius="16px" />
         </div>
     );
 });
